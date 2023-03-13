fbf5de8ae266b7e3df49643f9dbe3f6873441650
임지훈 <bo9701@naver.com>
2023-03-13 19:45:08 +0900

    practice

---
+++ Deep Learning/deep-learning.py
@@ -0,0 +1,8 @@
+asdlfj;lj
+asd;lfkj;j
+d;lj;lasdjf;ljkalsdfj
+jl;aksdjf;ljsld;fjl;sdjf
+jsadfl;ksdjf;lajsdf
+jasdl;kfj;alsdfj'aldjf
+kdksldkfkcnckdlsdnkdkxklslasdfooenlsdknosneoinlasala'sadfljljs
+asdlfkjljsoinncndskaskkxkksjdj
